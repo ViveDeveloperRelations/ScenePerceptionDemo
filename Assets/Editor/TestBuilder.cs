f556ebf856ec60d5023e5125600b47897ba92e33
Alex Sink <alexsink@yahoo.com>
2023-01-31 14:11:54 -0800

    cleanup, add aar as lfs files if needed

--- Assets/Editor/TestBuilder.cs
+++ Assets/Editor/TestBuilder.cs
@@ -7,106 +7,123 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class TestBuilder
-{
-    [MenuItem("TestBuilder/TestBuild")]
-    public static void Build()
     {
-		Debug.Log("Start build");
-        var buildNumberFaked = Convert.ToInt32(Random.Range(1f, 1000f));
-        try
+        [MenuItem("TestBuilder/TestBuild")]
+        public static void Build()
         {
-            //other axes potentially - unity version, scripting backend, .net api compatibility, rendering pipeline, old/new input, xr input(xri with xr rig and xr origin), other packages, C++ compiler config, release/debug, tbd
+			Debug.Log("Start build1");
+            var buildNumberFaked = Convert.ToInt32(Random.Range(1f, 1000f));
             var buildConfig = new BuildConfig()
-            {
-                BuildOptions = BuildOptions.Development, // | BuildOptions.AutoRunPlayer,
-                BuildTargetGroup = BuildTargetGroup.Android,
-                Scenes = BuildConfig.ScenesInApp(),
-                BundleVersionCode = buildNumberFaked, // PlayerSettings.Android.bundleVersionCode,
-                AppName = "Scene Perception "+buildNumberFaked+Application.unityVersion,
-                BundleIdentifier = "com.defaultCompany.sceneperception" +buildNumberFaked+ Application.unityVersion.Replace(".","_"),
-            };
-        
-            Builder.BuildAndroid(buildConfig);
-
+                {
+                    BuildOptions = BuildOptions.Development, // | BuildOptions.AutoRunPlayer,
+                    BuildTargetGroup = BuildTargetGroup.Android,
+                    Scenes = BuildConfig.ScenesInApp(),
+                    BundleVersionCode = buildNumberFaked, // PlayerSettings.Android.bundleVersionCode,
+                    AppName = "Scene Perception "+buildNumberFaked+Application.unityVersion,
+                    BundleIdentifier = "com.defaultCompany.sceneperception" +buildNumberFaked+ Application.unityVersion.Replace(".","_"),
+                };
+            BuildInternal(buildConfig);
         }
-        catch (Exception e)
-        {
-            Debug.LogError("Error during build");
-            Debug.LogException(e);
-        }
-        Debug.Log("End build");
-        if(Application.isBatchMode)
-            EditorApplication.Exit(0);
-    }
 
-    public class BuildConfig
-    {
-        public BuildOptions BuildOptions = BuildOptions.None;
-        public BuildTargetGroup BuildTargetGroup = BuildTargetGroup.Android;
-        public string AppName = $"ScenePerception{EscapedUnityVersion()}";
-        public string[] Scenes;
-        public string BundleIdentifier;
-        public int BundleVersionCode;
-
-        private static string EscapedUnityVersion()
+        [MenuItem("TestBuilder/TestBuild No Version update")]
+        public static void BuildNoVersionUpdate()
         {
-            return Application.unityVersion.Replace(".", "_");
+            var buildNumberFaked = Convert.ToInt32(Random.Range(1f, 1000f));
+            var buildConfig = new BuildConfig()
+                {
+                    BuildOptions = BuildOptions.Development, // | BuildOptions.AutoRunPlayer,
+                    BuildTargetGroup = BuildTargetGroup.Android,
+                    Scenes = BuildConfig.ScenesInApp(),
+                    BundleVersionCode = PlayerSettings.Android.bundleVersionCode,
+                    AppName = Application.productName,
+                    BundleIdentifier = Application.identifier,
+                };
+            BuildInternal(buildConfig);
         }
         
-        public static string[] ScenesInApp()
-        {
-#pragma warning disable CS0618
-            if (Application.levelCount == 0)
-#pragma warning restore CS0618
+        private static void BuildInternal(BuildConfig buildConfig){
+            Debug.Log("Start build");
+            try
             {
-                throw new InvalidOperationException("No levels set in player settings");
+                Builder.BuildAndroid(buildConfig);
             }
-            List<string> scenes = new List<string>();
-            foreach(var scene in EditorBuildSettings.scenes)
+            catch (Exception e)
             {
-                if(scene.enabled)
-                    scenes.Add(scene.path);
+                Debug.LogError("Error during build");
+                Debug.LogException(e);
             }
+            Debug.Log("End build");
+            if(Application.isBatchMode)
+                EditorApplication.Exit(0);    
+        }
 
-            return scenes.ToArray();
-        }
-        public bool IsValid()
+        public class BuildConfig
         {
-            return BuildTargetGroup != BuildTargetGroup.Unknown && 
-                   !string.IsNullOrEmpty(AppName) &&
-                   !string.IsNullOrEmpty(BundleIdentifier) && 
-                   Scenes != null && Scenes.Count((s => !string.IsNullOrEmpty(s))) >= 1; //at least one valid scene
-        }
-    }
-    public class Builder
-    {
-        private static string SafeWindowsFileName(string input)
-        {
-            char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+            public BuildOptions BuildOptions = BuildOptions.None;
+            public BuildTargetGroup BuildTargetGroup = BuildTargetGroup.Android;
+            public string AppName = $"VRTestApp{EscapedUnityVersion()}";
+            public string[] Scenes;
+            public string BundleIdentifier;
+            public int BundleVersionCode;
 
-            // Builds a string out of valid chars
-            var validFilename = new string(input.Where(ch => !invalidFileNameChars.Contains(ch)).ToArray());
-            return validFilename;
-        }
-        public static void BuildAndroid(BuildConfig Config)
-        {
-            if (Config == null || !Config.IsValid())
+            private static string EscapedUnityVersion()
             {
-                throw new ArgumentException("Invalid build config");
+                return Application.unityVersion.Replace(".", "_");
             }
-            PlayerSettings.SetApplicationIdentifier(Config.BuildTargetGroup,Config.BundleIdentifier);
-            PlayerSettings.productName = Config.AppName;
-            //Application.productName = AppName;
-            PlayerSettings.Android.bundleVersionCode = Config.BundleVersionCode;
             
-            var apkName =  $"{SafeWindowsFileName(Config.BundleIdentifier.Replace(".", "_"))}.apk";
-            const string buildDirName = "Builds";
-            if (!Directory.Exists(buildDirName))
-                Directory.CreateDirectory(buildDirName);
-            BuildPipeline.BuildPlayer(new BuildPlayerOptions()
+            public static string[] ScenesInApp()
             {
-                target = BuildTarget.Android, scenes = Config.Scenes, locationPathName = $"{buildDirName}/{apkName}",options = Config.BuildOptions
-            });
+#pragma warning disable CS0618
+                if (Application.levelCount == 0)
+#pragma warning restore CS0618
+                {
+                    throw new InvalidOperationException("No levels set in player settings");
+                }
+                List<string> scenes = new List<string>();
+                foreach(var scene in EditorBuildSettings.scenes)
+                {
+                    if(scene.enabled)
+                        scenes.Add(scene.path);
+                }
+
+                return scenes.ToArray();
+            }
+            public bool IsValid()
+            {
+                return BuildTargetGroup != BuildTargetGroup.Unknown && 
+                       !string.IsNullOrEmpty(AppName) &&
+                       !string.IsNullOrEmpty(BundleIdentifier) && 
+                       Scenes != null && Scenes.Count((s => !string.IsNullOrEmpty(s))) >= 1; //at least one valid scene
+            }
+        }
+        public class Builder
+        {
+            private static string SafeWindowsFileName(string input)
+            {
+                char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+
+                // Builds a string out of valid chars
+                var validFilename = new string(input.Where(ch => !invalidFileNameChars.Contains(ch)).ToArray());
+                return validFilename;
+            }
+            public static void BuildAndroid(BuildConfig Config)
+            {
+                if (Config == null || !Config.IsValid())
+                {
+                    throw new ArgumentException("Invalid build config");
+                }
+                PlayerSettings.SetApplicationIdentifier(Config.BuildTargetGroup,Config.BundleIdentifier);
+                PlayerSettings.productName = Config.AppName;
+                PlayerSettings.Android.bundleVersionCode = Config.BundleVersionCode;
+                
+                var apkName =  $"{SafeWindowsFileName(Config.BundleIdentifier.Replace(".", "_"))}.apk";
+                const string buildDirName = "Builds";
+                if (!Directory.Exists(buildDirName))
+                    Directory.CreateDirectory(buildDirName);
+                BuildPipeline.BuildPlayer(new BuildPlayerOptions()
+                {
+                    target = BuildTarget.Android, scenes = Config.Scenes, locationPathName = $"{buildDirName}/{apkName}",options = Config.BuildOptions
+                });
+            }
         }
     }
-}
--- localBuild.ps1
+++ localBuild.ps1
@@ -1,21 +1,26 @@
 #requires -Modules UnitySetup
 
-#$unityVersions = "2021.3.9f1"
+$unityVersions = "2021.3.9f1"
 #$unityVersions = "2020.3.18f1","2020.3.15f2"
 #2021.3.3f1 has issues with the installer, for instance it doesn't have a version string in the install so all automatic commands fail. you can run this manually from a path, but that is always ugly. also the installer fails with a cryptic message
 
-#foreach($unityVersion in $unityVersions)
-#{
-#    #install relevant version if not already present
-#    $alreadyHas = Get-UnitySetupInstance | Select-UnitySetupInstance -Version $unityVersion
-#    if(!$alreadyHas){
-#        Find-UnitySetupInstaller -Version $unityVersion -Components 'Android' | Install-UnitySetupInstance
-#    }
-
-#    #Start-UnityEditor -BuildTarget Android -ExecuteMethod TestBuilder.Build -BatchMode -Quit -LogFile ".\build$(get-date -f yyyy-MM-dd-hh-ss).log" -Wait -Version $unityVersion
-#}
-
-for (($i = 0); $i -lt 1; $i++)
+foreach($unityVersion in $unityVersions)
 {
-    Start-UnityEditor -BuildTarget Android -ExecuteMethod TestBuilder.Build -BatchMode -Quit -LogFile ".\build$(get-date -f yyyy-MM-dd-hh-ss).log" -Wait
+    #install relevant version if not already present
+    $alreadyHas = Get-UnitySetupInstance | Select-UnitySetupInstance -Version $unityVersion
+    if(!$alreadyHas){
+        Find-UnitySetupInstaller -Version $unityVersion -Components 'Android' | Install-UnitySetupInstance
+    }
+
+    #Start-UnityEditor -BuildTarget Android -ExecuteMethod TestBuilder.Build -BatchMode -Quit -LogFile ".\build$(get-date -f yyyy-MM-dd-hh-ss).log" -Wait -Version $unityVersion
+}
+foreach($unityVersion in $unityVersions)
+{
+    #build with each version
+    #run only version sthat installed successfully
+    $alreadyHas = Get-UnitySetupInstance | Select-UnitySetupInstance -Version $unityVersion
+    if(!$alreadyHas){
+        continue; #if installation of that version of unity failed, skip it
+    }
+    Start-UnityEditor -BuildTarget Android -ExecuteMethod TestBuilder.Build -BatchMode -Quit -LogFile ".\build$(get-date -f yyyy-MM-dd-hh-ss).log" -Wait -Version $unityVersion
 }
